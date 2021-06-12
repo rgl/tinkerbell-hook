0d4e3bfef436ac4e1777d3dd5cad2d6046540bc7
Rui Lopes <rgl@ruilopes.com>
2021-06-12 20:09:09 +0100

    send logs to loki

--- bootkit/main.go
+++ bootkit/main.go
@@ -43,9 +43,8 @@ type tinkConfig struct {
 func main() {
 	fmt.Println("Starting BootKit")
 
-	// // Read entire file content, giving us little control but
-	// // making it very simple. No need to close the file.
-
+	// Read entire file content, giving us little control but
+	// making it very simple. No need to close the file.
 	content, err := ioutil.ReadFile("/proc/cmdline")
 	if err != nil {
 		panic(err)
@@ -76,6 +75,7 @@ func main() {
 			fmt.Sprintf("ID=%s", cfg.workerID),
 			fmt.Sprintf("container_uuid=%s", cfg.MetadataID),
 		},
+		Labels:       map[string]string{"worker_id": cfg.workerID},
 		AttachStdout: true,
 		AttachStderr: true,
 	}
@@ -113,11 +113,6 @@ func main() {
 		RegistryAuth: authStr,
 	}
 
-	// Give time to Docker to start
-	// Alternatively we watch for the socket being created
-	time.Sleep(time.Second * 3)
-	fmt.Println("Starting Communication with Docker Engine")
-
 	// Create Docker client with API (socket)
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
@@ -125,7 +120,21 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Pulling image [%s]", imageName)
+	// Wait for docker to be ready.
+	fmt.Println("Waiting for docker to be ready")
+	for {
+		_, err := cli.Ping(ctx)
+		if err != nil {
+			if client.IsErrConnectionFailed(err) {
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			panic(err)
+		}
+		break
+	}
+
+	fmt.Printf("Pulling image [%s]\n", imageName)
 
 	out, err := cli.ImagePull(ctx, imageName, pullOpts)
 	if err != nil {
@@ -137,7 +146,7 @@ func main() {
 		panic(err)
 	}
 
-	resp, err := cli.ContainerCreate(ctx, tinkContainer, tinkHostConfig, nil, nil, "")
+	resp, err := cli.ContainerCreate(ctx, tinkContainer, tinkHostConfig, nil, nil, "tink-worker")
 	if err != nil {
 		panic(err)
 	}
--- tink-docker/main.go
+++ tink-docker/main.go
@@ -8,13 +8,16 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"syscall"
 	"time"
 )
 
 type tinkConfig struct {
-	registry   string
-	baseURL    string
-	tinkerbell string
+	registry         string
+	registryUsername string
+	registryPassword string
+	baseURL          string
+	tinkerbell       string
 
 	// TODO add others
 }
@@ -39,19 +42,160 @@ func main() {
 		panic(err)
 	}
 	// Download the configuration
+	fmt.Printf("Downloading the %s registry CA certificate\n", cfg.registry)
 	err = downloadFile(path+"ca.crt", cfg.baseURL+"/ca.pem")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Downloaded the repository certificates, starting the Docker Engine")
+
+	// // Install the docker plugins
+	// fmt.Println("Installing the Docker Engine plugins")
+	// err = installDockerPlugins(
+	// 	cfg.registry,
+	// 	cfg.registryUsername,
+	// 	cfg.registryPassword)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// Build the command, and execute
-	cmd := exec.Command("/usr/local/bin/docker-init", "/usr/local/bin/dockerd")
+	fmt.Println("Starting the Docker Engine")
+	cmd := exec.Command(
+		"/usr/local/bin/docker-init",
+		"-s",
+		"/usr/local/bin/dockerd")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func installDockerPlugins(registry string, registryUsername string, registryPassword string) error {
+	// execute dockerd in background.
+	// NB to install the plugins we need a running dockerd. we temporarily execute
+	//    one in background to install the plugins.
+	dockerHost := "unix:///dockerd.install.sock"
+	dockerConfigPath := "/dockerd.install.json"
+	err := ioutil.WriteFile(dockerConfigPath, []byte("{}"), 0644)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Starting up the temporary docker daemon")
+	cmd := exec.Command(
+		"/usr/local/bin/dockerd",
+		"--config-file",
+		dockerConfigPath,
+		"--host",
+		dockerHost)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	// wait for dockerd to be ready.
+	err = waitForDockerDaemon(dockerHost)
+	if err != nil {
+		return err
+	}
+
+	// login into the registry.
+	fmt.Printf("Logging in the %s registry\n", registry)
+	err = dockerLogin(dockerHost, registry, registryUsername, registryPassword)
+	if err != nil {
+		return err
+	}
+
+	// install the loki log driver plugin.
+	fmt.Println("Installing the loki log driver plugin")
+	err = installDockerPlugin(
+		dockerHost,
+		"loki",
+		fmt.Sprintf("%s/grafana/loki-docker-driver:2.3.0", registry),
+		"LOG_LEVEL=info")
+	if err != nil {
+		return err
+	}
+
+	// TODO how to properly wait?
+	fmt.Println("Waiting for the plugins to be ready")
+	time.Sleep(15 * time.Second)
+
+	// shutdown dockerd.
+	fmt.Println("Shutting down the temporary docker daemon")
+	cmd.Process.Signal(syscall.SIGTERM)
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	os.Remove(dockerConfigPath)
+
+	// TODO how to properly wait?
+	//      in theory, neither containerd nor the plugins are shutdown by docker, so this might be moot.
+	fmt.Println("Waiting for the containerd and docker plugins to shutdown")
+	time.Sleep(15 * time.Second)
+
+	return nil
+}
+
+func installDockerPlugin(dockerHost string, alias string, image string, arguments ...string) error {
+	cmd := exec.Command(
+		"/usr/local/bin/docker",
+		append(
+			[]string{
+				"--host",
+				dockerHost,
+				"plugin",
+				"install",
+				"--grant-all-permissions",
+				"--alias",
+				alias,
+				image,
+			},
+			arguments...)...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func dockerLogin(dockerHost string, registry string, username string, password string) error {
+	cmd := exec.Command(
+		"/usr/local/bin/docker",
+		"--host",
+		dockerHost,
+		"login",
+		"--username",
+		username,
+		"--password-stdin",
+		registry)
+	cmd.Stdin = strings.NewReader(password)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func waitForDockerDaemon(dockerHost string) error {
+	for {
+		cmd := exec.Command(
+			"/usr/local/bin/docker",
+			"--host",
+			dockerHost,
+			"info",
+			"--format",
+			"{{.ServerVersion}}")
+		err := cmd.Run()
+		if err == nil {
+			return nil
+		}
+		if _, ok := err.(*exec.ExitError); ok {
+			time.Sleep(1 * time.Second)
+		} else {
+			return err
+		}
 	}
 }
 
@@ -67,6 +211,10 @@ func parseCmdLine(cmdLines []string) (cfg tinkConfig) {
 		// Find Registry configuration
 		case "docker_registry":
 			cfg.registry = cmdLine[1]
+		case "registry_username":
+			cfg.registryUsername = cmdLine[1]
+		case "registry_password":
+			cfg.registryPassword = cmdLine[1]
 		case "packet_base_url":
 			cfg.baseURL = cmdLine[1]
 		case "tinkerbell":
